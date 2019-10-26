d0cba29d37a2dd40c838020779ccdf0590274662
zene1308 <42683201+zene1308@users.noreply.github.com>
2019-10-26 10:45:02 -0300

    commit javascript
    javascript

---
+++ JavaScript/exercicios/main.js
@@ -0,0 +1,22 @@
+//exercicio 1
+// function checaIdade(idade) {
+//     return new Promise(function (resolve, reject) {
+//         setTimeout(function(){
+//             if (idade > 18) {
+//                 resolve();
+//             } else {
+//                 reject();
+//             };    
+//         }, 2000);
+        
+//     });
+// }
+
+// checaIdade(18)
+//     .then(function () {
+//         console.log("maior que 18");
+//     })
+//     .catch(function () {
+//         console.log('menor que 18');
+//     });
+
